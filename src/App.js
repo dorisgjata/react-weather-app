3d0f9df3e1a8c04dd5d2fb89fbfffd33909220cc
dorisgjata <dorisgjata@oakland.edu>
2019-04-29 20:20:56 -0400

    Displaying current date

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Grommet, Text, Anchor, Button } from 'grommet';
+const theme = {
+	global: {
+		colors: {
+			blue: '#ADD8E6'
+		},
+		font: {
+			family: 'Roboto',
+			size: '14px',
+			height: '20px'
+		}
+	}
+};
+const today = new Date();
+const date = today.getDate();
+const day = today.getDay();
+const monthNum = today.getMonth();
+const year = today.getFullYear();
+const daysName = [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ];
+const monthsName = [
+	'January',
+	'February',
+	'March',
+	'April',
+	'May',
+	'June',
+	'July',
+	'August',
+	'September',
+	'October',
+	'November',
+	'December'
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const weekDay = daysName.find((item, index) => {
+	if (day === index + 1) return item;
+});
+const month = monthsName.find((item, index) => {
+	if (monthNum === index) return item;
+});
+const fullDate = date + ', ' + weekDay + ' ' + month + ' ' + year;
+export default class App extends React.Component {
+	render() {
+		console.log(date, weekDay, month, year, daysName);
+		return (
+			<Grommet theme={theme}>
+				<Box justify="center" direction="row-responsive" align="center" pad="large" gap="medium">
+					<Box
+						background="linear-gradient(to top,  #04befe   0%,  #209cff 100%)"
+						align="center"
+						pad="medium"
+						gap="medium"
+						round
+					>
+						<Box justify="center" direction="column-responsive">
+							<Box pad="xsmall">
+								<Text color="white">{fullDate}</Text>
+							</Box>
+						</Box>
+						<Text color="white">42*</Text>
+					</Box>
+				</Box>
+			</Grommet>
+		);
+	}
 }
-
-export default App;
